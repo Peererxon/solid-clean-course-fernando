974d481db41cb4202861d53516e51a6b0b5068f4
Anderson Gil <andersongil.dev@gmail.com>
2022-11-30 12:17:28 -0400

    Nombres para parametros y de funciones con interfaces

---
+++ src/clean-code/03-functions.ts
@@ -0,0 +1,35 @@
+(() => {
+  function getMovieById(movieId: string) {
+    console.log({ movieId });
+  }
+
+  function getMovieCastById(movieId: string) {
+    console.log({ id: movieId });
+  }
+
+  function getActorBiographyById(ActorId: string) {
+    console.log({ ActorId });
+  }
+
+  // Crear una película
+
+  interface movie {
+    cast: string[];
+    description: string;
+    rating: number;
+    title: string;
+  }
+  function createMovie({ cast, description, rating, title }: movie) {
+    console.log({ title, description, rating, cast });
+  }
+
+  function createActor(birthdate: Date, fullName: string): boolean {
+    // tarea asincrona para verificar nombre
+    // ..
+    // ..
+    if (fullName === "fernando") return false;
+
+    console.log("Crear actor");
+    return true;
+  }
+})();
